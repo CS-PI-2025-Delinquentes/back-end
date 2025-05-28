dd6fb2abf88f579b4cf30ea9fbba6a1a84e04313
Gregory Soares <gregory16704@gmail.com>
2025-05-28 09:50:17 -0300

    chore(geral): adicionando arquitetura de diretorios para o projeto

---
+++ src/main/java/com/pagil/teruel_express/TeruelExpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pagil.teruel_express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeruelExpressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeruelExpressBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagil/teruel_express/TeruelExpressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagil.teruel_express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeruelExpressBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
