29c5ec3b8468519766e95792d8ffae05a4c1f9cf
Gregory Soares <gregory16704@gmail.com>
2025-08-30 19:14:58 -0300

    feat(JWT): adicionando jwt, logica de autenticacao e seguranca

---
+++ src/main/java/com/pagil/teruel_express/jwt/JwtUserDetailsService.java
@@ -0,0 +1,48 @@
+package com.pagil.teruel_express.jwt;
+
+import com.pagil.teruel_express.exception.UsernameTypeException;
+import com.pagil.teruel_express.model.entity.Pessoa;
+import com.pagil.teruel_express.model.entity.PessoaFisica;
+import com.pagil.teruel_express.model.entity.PessoaJuridica;
+import com.pagil.teruel_express.service.PessoaFisicaService;
+import com.pagil.teruel_express.service.PessoaJuridicaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final PessoaFisicaService fisicaService;
+    private final PessoaJuridicaService juridicaService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username.length() == 11) {
+            PessoaFisica pessoa = fisicaService.buscarPorCpf(username);
+            return new JwtUserDetails(pessoa);
+        } else if (username.length() == 14) {
+            PessoaJuridica pessoa = juridicaService.buscarPorCnpj(username);
+            return new JwtUserDetails(pessoa);
+        }
+        else {
+            throw new UsernameTypeException("Username não é nem CPF nem CNPJ!");
+        }
+    }
+
+    public JwtToken getTokenAuthenticated(String username) {
+        if (username.length() == 11) {
+            Pessoa.Role role = fisicaService.buscarRolePorUsername(username);
+            return JwtUtils.createToken(username, role.name().substring("ROLE_".length()));
+        } else if (username.length() == 14) {
+            Pessoa.Role role = juridicaService.buscarRolePorUsername(username);
+            return JwtUtils.createToken(username, role.name().substring("ROLE_".length()));
+        }
+        else {
+            throw new UsernameTypeException("Username não é nem CPF nem CNPJ!");
+        }
+    }
+}
