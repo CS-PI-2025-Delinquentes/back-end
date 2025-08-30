039b62a36ec075b4da2913853ffdfc81ff72c199
Gregory Soares <gregory16704@gmail.com>
2025-08-29 21:02:52 -0300

    feat(classes): adicionado classes de pessoa, fisica e juridica

---
+++ src/main/java/com/pagil/teruel_express/model/entity/PessoaFisica.java
@@ -0,0 +1,24 @@
+package com.pagil.teruel_express.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class PessoaFisica extends Pessoa {
+    @Column(nullable = false, name = "nome")
+    private String nome;
+    @Column(unique=true, nullable=false, length=11, name = "cpf")
+    private String cpf;
+
+    public PessoaFisica() {
+        super();
+    }
+
+    public PessoaFisica(Long id, String email, String telefone, String senha, Role role, String nome, String cpf) {
+        super(id, email, telefone, senha, role);
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
