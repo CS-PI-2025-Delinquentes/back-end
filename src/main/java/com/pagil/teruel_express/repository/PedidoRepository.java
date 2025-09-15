07e4b68af9ba605925d9041974d5767a15cf58ec
Gregory Soares <gregory16704@gmail.com>
2025-09-15 11:10:00 -0300

    feat(pedido): adicionando findall paginado

--- src/main/java/com/pagil/teruel_express/repository/PedidoRepository.java
+++ src/main/java/com/pagil/teruel_express/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package com.pagil.teruel_express.repository;
 
 import com.pagil.teruel_express.model.entity.Pedido;
 import com.pagil.teruel_express.model.entity.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ import java.util.List;
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
     List<Pedido> findAllByPessoa(Pessoa pessoa);
+    Page<Pedido> findAllByPessoa(Pessoa pessoa, Pageable pageable);
 
 }
--- src/main/java/com/pagil/teruel_express/service/PedidoService.java
+++ src/main/java/com/pagil/teruel_express/service/PedidoService.java
@@ -1,9 +1,13 @@
 package com.pagil.teruel_express.service;
 
 import com.pagil.teruel_express.jwt.UserContextService;
+import com.pagil.teruel_express.model.entity.Cidade;
 import com.pagil.teruel_express.model.entity.Pedido;
+import com.pagil.teruel_express.repository.CidadeRepository;
 import com.pagil.teruel_express.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +21,9 @@ public class PedidoService {
     @Autowired
     private UserContextService userService;
 
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
     public Pedido insert(Pedido pedido) {
         return repository.save(pedido);
     }
@@ -27,6 +34,14 @@ public class PedidoService {
 
     public List<Pedido> findAll(){
         return repository.findAll();
+    }
+
+    public Page<Pedido> findAllPaged(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    public Page<Pedido> findAllByPessoaPaged(Pageable pageable) {
+        return repository.findAllByPessoa(userService.getCurrentPessoa(), pageable);
     }
 
 }
