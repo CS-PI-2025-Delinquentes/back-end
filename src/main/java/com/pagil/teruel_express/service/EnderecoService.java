2eda8d2fb901d1857e79ba5bb2844db9dfaaf483
Gregory Soares <gregory16704@gmail.com>
2025-10-06 16:02:15 -0300

    feat(endereco): add funcao para salvar ambos os endereços de uma vez

--- src/main/java/com/pagil/teruel_express/service/EnderecoService.java
+++ src/main/java/com/pagil/teruel_express/service/EnderecoService.java
@@ -7,9 +7,13 @@ import com.pagil.teruel_express.model.entity.Cidade;
 import com.pagil.teruel_express.model.entity.Endereco;
 import com.pagil.teruel_express.repository.CidadeRepository;
 import com.pagil.teruel_express.repository.EnderecoRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -25,6 +29,14 @@ public class EnderecoService {
         Cidade cidade = cidadeService.buscarPorNomeSeAtendida(dto.getCidade());
         Endereco endereco = EnderecoMapper.toEndereco(dto, cidade);
         return repository.save(endereco);
+    }
+
+    public List<Endereco> insertAll(EnderecoDTO origem, EnderecoDTO destino) {
+        Cidade cidadeOrigem = cidadeService.buscarPorNomeSeAtendida(origem.getCidade());
+        Endereco enderecoOrigem = EnderecoMapper.toEndereco(origem, cidadeOrigem);
+        Cidade cidadeDestino = cidadeService.buscarPorNomeSeAtendida(destino.getCidade());
+        Endereco enderecoDestino = EnderecoMapper.toEndereco(destino, cidadeDestino);
+        return repository.saveAll(Arrays.asList(enderecoOrigem, enderecoDestino));
     }
 
     public Endereco findById(Long id) {
