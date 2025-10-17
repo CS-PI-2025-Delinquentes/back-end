f881d14ef36990c749b184519ca37e8739e1ad93
Gregory Soares <gregory16704@gmail.com>
2025-10-17 20:42:48 -0300

    feat(login): separacao login

--- src/main/java/com/pagil/teruel_express/controller/AutenticacaoController.java
+++ src/main/java/com/pagil/teruel_express/controller/AutenticacaoController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -29,8 +30,24 @@ public class AutenticacaoController {
     private final AuthenticationManager authenticationManager;
     private final UserContextService contextService;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> autenticar(@RequestBody @Valid LoginDto dto, HttpServletRequest request) {
+    @PostMapping("/login/clientes")
+    public ResponseEntity<?> autenticarClientes(@RequestBody @Valid LoginDto dto, HttpServletRequest request) {
+        log.info("Processo de autenticação pelo login {}", dto.getCpfCnpj());
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.getCpfCnpj(), dto.getSenha());
+            authenticationManager.authenticate(authenticationToken);
+            JwtToken token = detailsService.getTokenAuthenticated(dto.getCpfCnpj());
+            return ResponseEntity.ok(token);
+        } catch (AuthenticationException ex) {
+            log.warn("Bad credentials from username {}", dto.getCpfCnpj());
+            return ResponseEntity.badRequest()
+                    .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais Inválidas"));
+        }
+    }
+
+    @PostMapping("/login/admin")
+    @PreAuthorize("hasAnyRole('ADMIN', 'DEV')")
+    public ResponseEntity<?> autenticarAdmin(@RequestBody @Valid LoginDto dto, HttpServletRequest request) {
         log.info("Processo de autenticação pelo login {}", dto.getCpfCnpj());
         try {
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.getCpfCnpj(), dto.getSenha());
